8b53a722ab8f2596f70ef288e358eaf05248ae30
Mostafa Moin <sa479374@gmail.com>
2024-01-14 19:12:09 +0600

    Jan 10 - 2024

---
+++ STARTERS 116/1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://www.codechef.com/problems/MINWAGE
+
+    int x; cin >> x;
+
+    if (x > 11) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
