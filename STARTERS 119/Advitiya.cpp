7a26a7cd01d6eba1e9a648415533aa6431d837c1
Mostafa Moin <sa479374@gmail.com>
2024-02-01 09:02:19 +0600

    solution

---
+++ STARTERS 119/Advitiya.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://www.codechef.com/problems/ADVITIYA1
+
+    int n; cin >> n;
+
+    if (n == 16 || n == 17 || n == 18) cout << "ADVITIYA" << endl;
+    else cout << "WAITING FOR ADVITIYA" << endl;
+
+    return 0;
+}
