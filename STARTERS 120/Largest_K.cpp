3e81653368008475da7e07d464a7c8f8f89bfbb7
Mostafa Moin <sa479374@gmail.com>
2024-02-09 08:50:00 +0600

    solution

---
+++ STARTERS 120/Largest_K.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://www.codechef.com/problems/LARGESTK343
+
+    w(x) {
+        int x, y; cin >> x >> y;
+
+        cout << y / (x - 1) << endl;
+    }
+
+    return 0;
+}
