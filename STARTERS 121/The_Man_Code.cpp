e711e07abf185cbd6724ce377396f9db7755a9aa
Mostafa Moin <sa479374@gmail.com>
2024-02-16 10:08:06 +0600

    solution

---
+++ STARTERS 121/The_Man_Code.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    // https://www.codechef.com/problems/MANCODE
+
+    w(x) {
+        int n; cin >> n;
+
+        cout << (n + 1) / 2 << " " << (n + 2) / 3 << endl;
+    }
+
+    return 0;
+}
