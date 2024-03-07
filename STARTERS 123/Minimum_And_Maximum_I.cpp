ec9776af9d92a0823d0650db22914b23af365873
Mostafa Moin <sa479374@gmail.com>
2024-03-07 07:55:12 +0600

    solution

--- STARTERS 123/Minimum_And_Maximum_I.cpp
+++ STARTERS 123/Minimum_And_Maximum_I.cpp
@@ -1,41 +1,32 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-typedef vector<pair<int, int>> vip;
-
-#define w(x)            int x;cin>>x;while(x--)
-#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ll              long long
-#define pb              push_back
-#define ff              first
-#define ss              second
-#define mp              make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-
-int main() {
-
-    Mostafa
-    // https://www.codechef.com/problems/MINANDMAX
-
-    w(x) {
-        int n; cin >> n;
-
-        ll a = n / 2;
-
-        ll ans = a * (a + 1);
-
-        if (n & 1) {
-            ans += (n / 2) + 1;
+const int mx = 100005;
+vector<int> adj[mx];
+int visited[mx];
+void bfs(int src) {
+    queue<int> q;
+    visited[src] = 1;
+    q.push(src);
+    while (!q.empty()) {
+        int head = q.front();
+        q.pop();
+        cout << head << ' ';
+        for (auto child : adj[head]) {
+            if (!visited[child]) {
+                visited[child] = 1;
+                q.push(child);
+            }
         }
-
-        cout << ans << endl;
     }
-
+}
+int main {
+    int nodes, edges; cin >> nodes >> edges;
+    for (int i = 0; i < edges; ++i) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int src = 1;
+    bfs(src);
     return 0;
 }
