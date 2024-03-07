34a475754097421ee59158ebb7d8ec3d14dd6208
Mostafa Moin <sa479374@gmail.com>
2024-03-07 12:37:04 +0600

    solution

---
+++ STARTERS 124/Cricket_Tournament.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int test; cin >> test;
+
+    while (test--) {
+        int n, m; cin >> n >> m;
+
+        if (n > m) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+}
