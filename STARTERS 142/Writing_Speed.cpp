cdc9770ca7e0c8675e3f4c215055c3b44ec1ded3
Mostafa Al Moin <sa479374@gmail.com>
2024-07-13 09:46:22 +0600

    solution

---
+++ STARTERS 142/Writing_Speed.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define all(x) (x).begin(), (x).end()
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+bool comp(int a, int b) {return a > b;}
+
+int main() {
+	Mostafa
+
+	// https://www.codechef.com/problems/WRITINGSPEED
+
+	int x; cin >> x;
+
+	if (x * 5 <= 60) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
